f5ac2d2d5c298c727681a98646b7ccba61594727
DevAman-sudo <devaman.net@gmail.com>
2021-08-10 22:59:01 +0545

    on development

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const loader = document.getElementById("loader");
+// const body = document.getElementById('body');
+
+setTimeout(() => {
+  loader.style.display = "none";
+//   body.style.backgroundColor = '#ffffff';
+}, 2000);

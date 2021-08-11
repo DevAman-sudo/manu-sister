3aa88d6f4357cd285c03eb2768724793d8ae3d50
DevAman-sudo <devaman.net@gmail.com>
2021-08-11 08:38:52 +0545

    photo push

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 const loader = document.getElementById("loader");
-// const body = document.getElementById('body');
 
 setTimeout(() => {
   loader.style.display = "none";
-//   body.style.backgroundColor = '#ffffff';
-}, 2000);
+}, 3000);
